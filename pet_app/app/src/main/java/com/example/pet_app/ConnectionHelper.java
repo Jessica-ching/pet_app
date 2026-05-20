6b18c0426344442478c7a581d12a77cbc420482d
Jessica-ching <A11423030@yuntech.edu.tw>
2026-05-21 05:38:45 +0800

    Update ConnectionHelper.java

--- pet_app/app/src/main/java/com/example/pet_app/ConnectionHelper.java
+++ pet_app/app/src/main/java/com/example/pet_app/ConnectionHelper.java
@@ -10,7 +10,7 @@ public class ConnectionHelper {
         // Azure 專用的連線字串格式
         // 1. 伺服器位址後加上 :1433
         // 2. ssl=request 確保加密通訊
-        String connUrl = "jdbc:jtds:sqlserver://petlove.database.windows.net:1433;" +
+        String connUrl = "jdbc:jtds:sqlserver://192.168.43.55:1433;" +
                 "databaseName=PetDB;" +
                 "user=petapp_admin@petlove;" + // 🌟 注意：Azure 有時需要 @伺服器名
                 "password=groupH115;" +
